73ffe7fedb07a4bd87e3a0a19403b2fc55085afd
Jean Celin <jean_estudos@hotmail.com>
2024-09-18 10:12:50 -0300

    some styles changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Button from "@/components/navigation/Button";
-import styles from "@/styles/pages/home/Home.module.css";
+import styles from "@/styles/pages/Home.module.css";
 
 export default function Home() {
   return (
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,18 +1,22 @@
+import styles from "@/styles/pages/Menu.module.css";
+
 export default function Menu() {
   return (
-    <div>
-      <h1>Menu</h1>
-      <ul>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-      </ul>
+    <div id={styles.menu}>
+      <div className={styles.menu_content}>
+        <h1>Menu</h1>
+        <ul>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+          <li>Item</li>
+        </ul>
+      </div>
     </div>
   );
 }
